7446d87e4759df34309782e51888891b286f6d6d
Jonathan Nygaard <jonathan.nygaard@no.experis.com>
2026-01-28 13:39:33 +0100

    Finished UserRepository

--- backend/Api/Repositories/UserRepository.cs
+++ backend/Api/Repositories/UserRepository.cs
@@ -7,38 +7,47 @@ namespace Api.Repositories;
 
 public class UserRepository : IUserRepository
 {
-    private readonly AppDbContext _dbContext;
+    private readonly AppDbContext _context;
     
-    public UserRepository(AppDbContext dbContext)
+    public UserRepository(AppDbContext context)
     {
-        _dbContext = dbContext;
+        _context = context;
     }
     
-    public Task<IEnumerable<User>> GetAll()
+    public async Task<IEnumerable<User>> GetAll()
     {
-        throw new NotImplementedException();
+        return await _context.Users.AsNoTracking().ToListAsync();
     }
     
     public async Task<User> Create(User item)
     {
-        await _dbContext.Users.AddAsync(item);
+        await _context.Users.AddAsync(item);
+        await _context.SaveChangesAsync();
         return item;
     }
     
     public Task<User?> Read(int id)
     {
-        return _dbContext.Users.FirstOrDefaultAsync(user => user.Id == id);
+        return _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+    }    
+    public async Task<bool> Update(int Id, User item)
+    {
+        var existing = await _context.Users.FirstOrDefaultAsync(u => u.Id == Id);
+        
+        //Add more changes depending on what user contains
+        existing.UserName = item.UserName;
+        
+        var affected  = await _context.SaveChangesAsync();
+        return affected > 0;
     }
     
-    public Task<bool> Delete(int id)
+    public async Task<bool> Delete(int id)
     {
-        var _user = _dbContext.Users.Find(id);
-        _dbContext.Users.Remove(_user);
-        return Task.FromResult(true);
+        var _user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
+        _context.Remove(_user);
+        var affected  = await _context.SaveChangesAsync();
+        return affected > 0;
     }
     
-    public Task<bool> Update(int Id, User item)
-    {
-        throw new NotImplementedException();
-    }
+
 }
