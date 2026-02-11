a98b07605b953cd2cf0c1aedae457d01718508d7
Lan Nguyen Pham <quynhlanp@gmail.com>
2026-02-11 08:08:45 +0100

    Created TVShowCard component and used in home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { TVShowCard } from "../components/TVShowCard";
+
 
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  const recentEntries = [{
+    id: "1",
+    title: "Spider-Man",
+    posterUrl: "https://original.fontsinuse.com/fontsinuse.com/use-images/165/165448/165448.jpeg",
+    rating: 4,
+    releaseYear: 2002,
+    genre: "Action"
+  },
+  {
+    id: "2",
+    title: "Spider-Man 2",
+    posterUrl: "https://www.movieposters.com/cdn/shop/products/2a27abf44e604b54903eb8e2beba66eb_2fafebce-cc32-41e1-a559-2b078d0f15f6_grande.jpg?v=1762513052",
+    rating: 4,
+    releaseYear: 2004,
+    genre: "Action"
+  }]
+
+
+  return (
+    <>
+      <div>
+        <h2>Recent diary</h2>
+        {recentEntries.length === 0 ?
+          <p>No diary entries yet. Add your first show!</p>
+          :
+          recentEntries.map(e => <TVShowCard tvShow={e} />)
+        }
+      </div>
+    </>
+
+  )
 };
 
 export default Home;
