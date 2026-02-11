e394ffb37f9273091b3226c092a190b774ba8d97
Nikolai Gangstø <nikolai.gangstoe@gmail.com>
2026-02-11 09:28:41 +0100

    vite proxy added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env variables
+  const env = loadEnv(mode, process.cwd(), "")
+  console.log("Proxy target:", env.VITE_API_URL)
+
+  return {
+    plugins: [react()],
+
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        }
+      }
+    }
+  }
 })
