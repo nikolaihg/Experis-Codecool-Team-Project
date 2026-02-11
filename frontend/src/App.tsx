4d39a6576ed40531b75dd3550a02c2964641f45b
Nikolai Gangstø <nikolai.gangstoe@gmail.com>
2026-02-11 11:06:19 +0100

    added notfound

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,31 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
+
+import Home from './pages/Home'
+import About from './pages/About'
 import WatchList from './pages/WatchList'
 import Login from './pages/login'
 import Register from './pages/register'
-
+import NotFound from './components/NotFound'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/watchlist" element={<WatchList />} />
-    </ Routes>
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/watchlist" element={<WatchList />} />
+        <Route path="*" element={<NotFound />}></Route>
+      </ Routes>
+
+      <Footer />
+    </>
   )
 }
 
