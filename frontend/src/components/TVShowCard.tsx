49d39d332c66e1e471f213fe308c2700969ec737
Jonathan Nygaard <jonathan.nygaard@no.experis.com>
2026-02-12 13:39:22 +0100

    Trying again

--- frontend/src/components/TVShowCard.tsx
+++ frontend/src/components/TVShowCard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styles from "./TVShowCard.module.css"
 import { AddTvShow } from './addTvShow';
 // import './App.css'
